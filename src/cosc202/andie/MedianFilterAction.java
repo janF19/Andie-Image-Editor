25a37b0e1449ad88c053e155bd4475efe8e60868
jan988 <jan988@users.noreply.github.com>
2024-03-20 15:16:54 +1300

    Errors resolved, had to put MedianFilterAction class to separate file, no idea why, everything wokrs now

---
+++ src/cosc202/andie/MedianFilterAction.java
@@ -0,0 +1,76 @@
+package cosc202.andie;
+
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+/**
+     * <p>
+     * Action to blur an image with a median filter.
+     * </p>
+     * 
+     * @see MedianFilter
+     * 
+     * @Jan Faller
+     * 
+     * 
+     */
+
+
+public class MedianFilterAction extends ImageAction{
+
+
+    /**
+     * <p>
+     * Create a new median-filter action.
+     * </p>
+     * 
+     * @param name The name of the action (ignored if null).
+     * @param icon An icon to use to represent the action (ignored if null).
+     * @param desc A brief description of the action  (ignored if null).
+     * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+     */
+    MedianFilterAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+        super(name, icon, desc, mnemonic);
+    }
+
+    /**
+     * <p>
+     * Callback for when the convert-to-grey action is triggered.
+     * </p>
+     * 
+     * <p>
+     * This method is called whenever the MedianFilterAction is triggered.
+     * It prompts the user for a filter radius, then applies an appropriately sized {@link MedianFilter}.
+     * </p>
+     * 
+     * @param e The event triggering this callback.
+     */
+
+     public void actionPerformed(ActionEvent e){
+        int radius = 1;
+
+        SpinnerNumberModel radiusModelMedian = new SpinnerNumberModel(1,1,10,1);
+        JSpinner radiusSpinner = new JSpinner(radiusModelMedian);
+        int option = JOptionPane.showOptionDialog(null, radiusSpinner, "Enter filter radius", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+        // Check the return value from the dialog box.
+        if (option == JOptionPane.CANCEL_OPTION) {
+            return;
+        } else if (option == JOptionPane.OK_OPTION) {
+            radius = radiusModelMedian.getNumber().intValue();
+        }
+
+        // Create and apply the filter
+        target.getImage().apply(new MedianFilter(radius));
+        target.repaint();
+        target.getParent().revalidate();
+
+
+
+     }
+
+
+
+ }
