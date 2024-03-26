f471e9af52922e58b3dceec4db6d57f51da79e06
frada535 <frada535@student.otago.ac.nz>
2024-03-26 13:53:11 +1300

    starting comments

--- src/cosc202/andie/ImageInversion.java
+++ src/cosc202/andie/ImageInversion.java
@@ -3,12 +3,45 @@ package cosc202.andie;
 
 import java.awt.image.*;
 
-
+/**
+ * <p>
+ * ImageOperation to invert the colours from the original image.
+ * </p>
+ * 
+ * <p>
+ * The images produced by this operation are flipped from
+ * their original state.
+ * </p>
+ * 
+ * 
+ * @author Damian Fraser
+ * @version 1.0
+ */
 public class ImageInversion {
     public class InvertColours implements ImageOperation, java.io.Serializable {
+        /**
+         * <p>
+         * Creates a new InvertColours operation.
+         * </p>
+         */
         InvertColours() {
         }
 
+
+        /**
+         * <p>
+         * Applies the invert colour to an image.
+         * </p>
+         * 
+         * <p>
+         * The inverting of the images colours, uses a nested for loop to get the height
+         * and width of the image. Then takes and stores the red, blue, and green values
+         * of the image, then does 255-colour (red, blue, and green) to invert the colours.
+         * </p>
+         * 
+         * @param input The image to have its colours inverted.
+         * @return The resulting image after having its colours inverted.
+         */
         public BufferedImage apply(BufferedImage input) {
             int width = input.getWidth();
             int height = input.getHeight();
@@ -19,7 +52,7 @@ public class ImageInversion {
                     int green = (rgb >> 8) & 0xFF;
                     int blue = rgb & 0XFF;
 
-
+                    // the inverting of the colours for red, green, and blue
                     red = 255 - red;
                     green = 255-green;
                     blue = 255-blue;
--- src/cosc202/andie/ImageRotate.java
+++ src/cosc202/andie/ImageRotate.java
@@ -2,12 +2,47 @@ package cosc202.andie;
 
 import java.awt.image.*;
 
+/**
+ * <p>
+ * ImageRotate to rotate the image based on the user choice.
+ * </p>
+ * 
+ * <p>
+ * The images produced by this operation are rotated either 90 
+ * degrees left, 90 degrees right, or 180 degrees based on the 
+ * users choice when prompted.
+ * </p>
+ * 
+ * 
+ * @author Damian Fraser
+ * @version 1.0
+ */
 public class ImageRotate {
     public class Rotate implements ImageOperation {
         private int degrees;
+        /**
+         * <p>
+         * Creates a new Rotate operation and initializes and int degrees
+         * which represents the roatation the user has chosen when prompted.
+         * </p>
+         */
         public Rotate(int degrees){
             this.degrees = degrees;
         }
+        /**
+         * <p>
+         * Applies the colour channel cycle to an image.
+         * </p>
+         * 
+         * <p>
+         * The cycling of the images colours, uses a nested for loop to get the height
+         * and width of the image. Then takes and stores the red, blue, and green values
+         * of the image, then cycles the values based on the users choice.
+         * </p>
+         * 
+         * @param input The image to have its colours cycled.
+         * @return The resulting image after having its colours cycled.
+         */
         public BufferedImage apply(BufferedImage input){
             int width = input.getWidth();
             int height = input.getHeight();  
