e4f019f9dd4c3c5614e97d1bc6b9d6600369316e
frada535 <frada535@student.otago.ac.nz>
2024-05-08 16:37:07 +1200

    finish crop. need to fill out image and fix toolba

---
+++ src/cosc202/andie/CropActions.java
@@ -0,0 +1,66 @@
+package cosc202.andie;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
+
+
+public class CropActions {
+    protected ArrayList<Action> actions;
+
+    public CropActions(){
+        actions = new ArrayList<Action>();
+        actions.add(new CropAction(LanguageActions.prefs.getString("CropImage"), null, "CropImage", Integer.valueOf(KeyEvent.VK_L)));
+    }
+
+    /**
+     * <p>
+     * Create a menu containing the list of Crop actions.
+     * </p>
+     * 
+     * @return The crop menu UI element.
+     */
+    public JMenu createMenu() {
+        JMenu cropMenu = new JMenu(LanguageActions.prefs.getString("Crop"));
+
+        for (Action action : actions) {
+            cropMenu.add(new JMenuItem(action));
+        }
+
+        return cropMenu;
+    }
+
+    public class CropAction extends ImageAction{
+
+        CropAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+        public void actionPerformed(ActionEvent e) {
+            target.addMouseListener(new MouseAdapter(){
+                int x1, y1,x2,y2;
+                public void mousePressed(MouseEvent e) {
+                    x1 = e.getX();
+                    y1 = e.getY();
+                }
+
+                public void mouseReleased(MouseEvent e) {
+                    x2 = e.getX();
+                    y2 = e.getY();
+                    int width = Math.abs(x2 - x1);
+                    System.out.println("width is " + width);
+                    int height = Math.abs(y2 - y1);
+                    int topLeftX = Math.min(x1, x2);
+                    int topLeftY = Math.min(y1, y2);                          
+                    target.getImage().apply(new CropImage(topLeftX, topLeftY, topLeftX+width, topLeftY+height));
+                    target.repaint();
+                    target.removeMouseListener(this);
+                }
+            });   
+        }
+    }
+}
--- src/cosc202/andie/CropImage.java
+++ src/cosc202/andie/CropImage.java
@@ -15,20 +15,26 @@ import java.awt.image.BufferedImage;
  * @author Damian Fraser
  * @version 1.0
  */
-public class CropImage {
-    public static BufferedImage apply(BufferedImage originalImage, int x1, int y1, int x2, int y2) {
-        int x = Math.min(x1, x2);
-        int y = Math.min(y1, y2);
-        int width = x2 - x1;
-        int height = y2 - y1;
+public class CropImage implements ImageOperation{
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
 
-        BufferedImage croppedImage = new BufferedImage(width, height, originalImage.getType());
+    public CropImage(int x1, int y1, int x2, int y2){
+        this.x1=x1;
+        this.y1=y1;
+        this.x2=x2;
+        this.y2=y2;
+    }
+    public BufferedImage apply(BufferedImage input) {
+        int x= Math.min(x1,x2);
+        int y= Math.min(y1,y2);
+        int width = Math.abs(x2-x1);
+        int height = Math.abs(y2-y1);
 
-        Graphics2D g2d = croppedImage.createGraphics();
-        g2d.drawImage(originalImage, 0, 0, width, height, x, y, x + width, y + height, null);
-        g2d.dispose();
-
-        return croppedImage;
+        return input.getSubimage(x,y,width,height);
     }
     
 }
+
