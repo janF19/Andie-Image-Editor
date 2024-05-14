44a333af72a22c202e6d7c2f7337f3ffa8a82142
Maiek Anantawat <anama797@student.otago.ac.nz>
2024-05-14 14:29:33 +1200

    added comments to the vibrance filter

--- src/cosc202/andie/Vibrance.java
+++ src/cosc202/andie/Vibrance.java
@@ -21,10 +21,35 @@ public class Vibrance implements ImageOperation {
 
     int vibranceLevel = 0;
 
+    /** 
+     * <p> 
+     * Creates a new Vibrance operation and initializes an int 
+     * vibranceLevel which represents how much the user wants to 
+     * change the vibrance of the image by based on the users choice 
+     * when prompted 
+     * </p> 
+     */
     public Vibrance(int vibranceLevel) {
         this.vibranceLevel = vibranceLevel;
     }
 
+    /** 
+     * <p> 
+     * Applies the vibrance change to the image. 
+     * </p> 
+     * 
+     * <p> 
+     * The change in vibrance for the image, use a nested for loop to 
+     * get the height and width of the image. It then stores and converts the 
+     * images colors from RGB(red, green, blue) to HSB(hue, saturation, brightness), 
+     * calculates the current saturation level of the image and uses that to calculate 
+     * how much to multiply the vibrance by which then multiplies the saturation by that amount.
+     * It then converts the HSB back to RGB and sets the RGB values of the image to the new values. 
+     * </p> 
+     * 
+     * @param input The image to have its vibrance changed 
+     * @return The resulting image after having its vibrance changed 
+     */
     public BufferedImage apply(BufferedImage input) {
         int width = input.getWidth();
         int height = input.getHeight();
