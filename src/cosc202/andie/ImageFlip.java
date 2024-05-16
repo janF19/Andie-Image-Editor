6881d19f7fcfa11e367ea854c30e6fe17de03229
Parsa Orodes <jampa842@student.otago.ac.nz>
2024-05-17 10:09:05 +1200

    comments

--- src/cosc202/andie/ImageFlip.java
+++ src/cosc202/andie/ImageFlip.java
@@ -2,7 +2,6 @@ package cosc202.andie;
 
 import java.awt.image.*;
 
-
 /**
  * A class to flip images vertically or horizontally to create a mirrored
  * image along the relevant axis.
@@ -11,6 +10,12 @@ import java.awt.image.*;
  */
 public class ImageFlip {
 
+    /**
+     * A class to flip images vertically or horizontally to create a mirrored
+     * image along the relevant axis.
+     * 
+     * @author Noah Parkes
+     */
     public class Flip implements ImageOperation {
         /**
          * Determines if vertical (true)
@@ -21,41 +26,42 @@ public class ImageFlip {
         /**
          * Constructor that takes a boolean as perameter
          * 
-         * @param vertical boolean value. True if flip is to be vertical, false if horizontal
+         * @param vertical boolean value. True if flip is to be vertical, false if
+         *                 horizontal
          */
-        public Flip(boolean vertical){
+        public Flip(boolean vertical) {
             this.vertical = vertical;
         }
 
         /**
-         * Carries out and applies flip operation input image 
+         * Carries out and applies flip operation input image
          * 
          * @param input image to be flipped
          * @return now-flipped image
-        */
-        public BufferedImage apply(BufferedImage input){
+         */
+        public BufferedImage apply(BufferedImage input) {
             int width = input.getWidth();
-            int height = input.getHeight();  
+            int height = input.getHeight();
             BufferedImage flippedImage = new BufferedImage(width, height, input.getType());
 
-            if(vertical){
-                for(int y = 0; y < height; y++){
-                    for(int x = 0; x < width; x++){
-                        int newY = height - y - 1; //sets new y value to be the mirror coordinate of y
-                                                   //must also subtract 1, as image indexes from 0
-                        flippedImage.setRGB(x, newY ,input.getRGB(x,y));    
+            if (vertical) {
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int newY = height - y - 1; // sets new y value to be the mirror coordinate of y
+                                                   // must also subtract 1, as image indexes from 0
+                        flippedImage.setRGB(x, newY, input.getRGB(x, y));
                     }
                 }
-            }else{
-                for(int y = 0; y < height; y++){
-                    for(int x = 0; x < width; x++){
+            } else {
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
                         int newX = width - x - 1;
-                        flippedImage.setRGB(newX, y,input.getRGB(x,y));    
+                        flippedImage.setRGB(newX, y, input.getRGB(x, y));
                     }
                 }
             }
-            
-            return flippedImage;  
+
+            return flippedImage;
         }
     }
 }
